bb4fce59e02ed105adf4ff9db15aa896c5ca4920
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-04-30 16:18:16 -0400

    fixed graph

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 //set variable for dice
 let count= ['','',0,0,0,0,0,0,0,0,0,0,0];
 
-
-let numberoutput= document.createElement("div");
-
-
-
 function roll(){
     for (let i = 1; i <= 1000; i++){
-       let sum = diceroll() + diceroll();
+       let sum = diceRoll() + diceRoll();
        count[sum] += 1;
        
     }
     displayCount();
 }
 function displayCount(){ 
+    let mainDiv = document.getElementById("mainDiv")
     for (let i = 2; i < count.length; i++){
+        let div2 = document.createElement("div");
+        //Creating the p element
         var p1 = document.createElement("p");
-        let textnode= document.createTextNode(i+ ": " + count[i]);
+        let textnode= document.createTextNode(i + ": " + count[i]);
         p1.appendChild(textnode);
-        numberoutput.appendChild(p1);
-        console.log(p1);
-        
+        //Creating a div element to display a graph
+        let div3 = document.createElement("div");
+        div3.className = "graph";
+        div3.style.width=count[i]+"px";
+        div2.appendChild(p1);
+        div2.appendChild(div3)
+
+        mainDiv.appendChild(div2)
+        // numberoutput.appendChild(p1);
+        console.log(p1);       
     }
-    document.body.appendChild(numberoutput);
+   
+
 }
-function diceroll(){
+function diceRoll(){
     let dice = Math.floor(Math.random()*6 + 1);
     return dice;
 }
-roll()
+
+roll();
 
 
 
